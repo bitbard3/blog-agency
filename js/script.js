54191be1e14d010b535687ada615c4dc900813ae
bitbard3 <ansharora3839@gmail.com>
2023-10-18 10:16:58 +0530

    Add and style the website details section

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-// Add custom JavaScript here
+function navScroll(){
+    const navbar = document.querySelector('.navbar')
+
+    window.addEventListener('scroll',() =>{
+        if(window.scrollY>50){
+            navbar.classList.add('bg-dark');
+            navbar.classList.add('nav-sticky');
+        }
+        else{
+            navbar.classList.remove('bg-dark');
+            navbar.classList.remove('nav-sticky');
+        }
+    });
+
+}
+
+document.addEventListener('DOMContentLoaded',navScroll);
